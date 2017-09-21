b592416d5c41318698ea56e4fddc4b657a43c87a
MichaelVara <32087977+MichaelVara@users.noreply.github.com>
2017-09-21 12:49:47 +0200

    Add files via upload

---
+++ Source/Controle/Controle.ino
@@ -0,0 +1,40 @@
+// Nom: Projet 2
+// Auteur: Michael Vara
+// Date: 21/09/2017
+// Description: Controle du drone a l'aide de des joysticks
+
+
+#include <CoDrone.h>
+
+void setup() {
+  // Connexion avec le drone
+  CoDrone.begin(115200);
+  byte droneAddress[6] = {0x14, 0xB9, 0x60, 0x78, 0xD5, 0xA4};
+  CoDrone.AutoConnect(AddressInputDrone, droneAddress);
+
+  // Demarrer les moteurs
+  CoDrone.FlightEvent(TakeOff);
+}
+
+void loop() {
+  if(PAIRING == true){
+    // Joystick droit
+        // Monter / descendre
+        THROTTLE = CoDrone.AnalogScaleChange(analogRead(25));
+        // Tourner
+        YAW = 0 - CoDrone.AnalogScaleChange(analogRead(24));
+
+    // Joystick gauche
+        // Avance / reculer
+        PITCH = CoDrone.AnalogScaleChange(analogRead(23));
+        ROLL = 0 - CoDrone.AnalogScaleChange(analogRead(22));
+    CoDrone.Control();
+
+    if(digitalRead(11))
+    {
+      // Atterrissage du drone
+      CoDrone.FlightEvent(Landing);
+    }
+  }
+}
+
